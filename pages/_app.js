b53d940eefad8fa054086b832c5fc5c421c58aa9
brandonsterling <brandonsterling@knights.ucf.edu>
2023-01-26 00:40:53 -0500

    Fixing direction bug

--- pages/_app.js
+++ pages/_app.js
@@ -38,7 +38,7 @@ export default function App(props) {
   const Layout = Component.Layout || ((children) => <>{children}</>);
 
   return (
-    <>
+    <div>
       <Head>
         <title>Page title</title>
         <meta
@@ -90,6 +90,6 @@ export default function App(props) {
           <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </SessionContextProvider>
-    </>
+    </div>
   );
 }
