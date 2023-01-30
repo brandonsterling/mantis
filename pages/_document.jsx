a1b333fe059321da8a2ee4200ff436c7257b0c2c
brandonsterling <brandonsterling@knights.ucf.edu>
2023-01-30 14:56:07 -0500

    Updating document file to fix fouc issue

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,8 +1,28 @@
 import Document from "next/document";
-import { createGetInitialProps } from "@mantine/next";
+import {
+  createGetInitialProps,
+  createStylesServer,
+  ServerStyles,
+} from "@mantine/next";
 
-const getInitialProps = createGetInitialProps();
+const stylesServer = createStylesServer();
 
 export default class _Document extends Document {
-  static getInitialProps = getInitialProps;
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    // Add your app specific logic here
+
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <ServerStyles
+          html={initialProps.html}
+          server={stylesServer}
+          key="styles"
+        />,
+      ],
+    };
+  }
 }
